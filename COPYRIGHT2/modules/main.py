fb4b43cdf96c057a064fd5edf95bc0bb577a1852
Payal8800 <124197031+Payal8800@users.noreply.github.com>
2024-04-22 19:24:53 +0530

    Update main.py

--- COPYRIGHT2/modules/main.py
+++ COPYRIGHT2/modules/main.py
@@ -50,7 +50,7 @@ async def start(_, msg):
 gd_buttons = [              
         [
             InlineKeyboardButton("ᴏᴡɴᴇʀ", user_id=OWNER_ID),
-            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/VillainBots),    
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/VillainBots"),    
         ]
         ]
 
